eaaf10a6ad3034c13c6141bf470b179151ff39e7
Anisa Tahlil <anisa.tahlil22@gmial.com>
2019-01-14 16:41:33 +0000

    added modularity step 3

---
+++ step3.js
@@ -0,0 +1,41 @@
+var https = require('https');
+
+var options = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+
+function getAndPrintHTML (options) {
+
+  // var requestOptions = {
+  //   host: options['host'],
+  //   path: options['path']
+  // };
+
+  https.get(options, function (response) {
+
+  // set encoding of received data to UTF-8
+  response.setEncoding('utf8');
+
+  // the callback is invoked when a `data` chunk is received
+
+
+response.on('data', function (data) {
+     var respContent = '' ;
+     respContent += data.toString() ;//data is a buffer instance
+     console.log(respContent);
+  });
+
+
+  // the callback is invoked when all of the data has been received
+  // (the `end` of the stream)
+  response.on('end', function() {
+    console.log('Response stream complete.');
+  });
+
+});
+
+}
+
+getAndPrintHTML(options);
